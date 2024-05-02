d0029d0c2bd1a5ff50eabde6d93ce30c68dfb8bf
inessashun <inshun2018@gmail.com>
2024-05-02 07:48:00 +0300

    add-new-const

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  #set screen size
 
 pygame.display.set_caption("Game Target")     #screen name
-icon = pygame.image.load("img/target.jpg")    #screen image
+icon = pygame.image.load("img/аватар.jpg")    #screen image
 pygame.display.set_icon(icon)                 #set icon
 
-target_img = pygame.image.load("img/")
-target_width = 50
-target_height = 50
+target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
 
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 running = True
